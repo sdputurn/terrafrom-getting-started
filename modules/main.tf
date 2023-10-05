76d54aaeece1f332633ef60943033d129843e0ab
i517528 <sandeep.singh03@sap.com>
2023-10-05 23:29:09 +0530

    getting started commited

--- modules/main.tf
+++ modules/main.tf
@@ -2,13 +2,26 @@ variable "repo_name" {
     default = "terrafrom-module-getting-started"
   
 }
+variable username {}
+
 variable "description" {
     default = "terrafrom module basics"
   
 }
 resource "github_repository" "learnings" {
     name = var.repo_name
-    description = var.description
+    description = format("%s - %s", var.description, var.username )
     auto_init = true
     visibility = "public"  
+}
+resource "github_repository" "learnings2" {
+    name = format("%s-%s", var.repo_name , "two")
+    description = var.description 
+    auto_init = true
+    visibility = "public"  
+}
+
+output "going_crazy" {
+  value = "${github_repository.learnings.html_url} ++++++++ ${github_repository.learnings2.html_url}" 
+  
 }
