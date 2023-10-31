70f02f8980a84ead07b3a22cae4d53f967838dde
sdputurn <sdp.uturn@gmail.com>
2023-10-31 12:40:18 +0530

    module added

--- main.tf
+++ main.tf
@@ -1,22 +1,14 @@
-variable "username" {
-  default = "sandeep"
-}
 variable "token" {
-#   default = "free"
-}
-output "tty" {
-  value = github_repository.getting-started.html_url
-
-
+    # default = var.token
+  
 }
 provider "github" {
     token = var.token
-    # reading from env variable
-
+  
 }
-resource "github_repository" "getting-started" {
-  name = "terrafrom-getting-started"
-  auto_init = true
-  description = "learning terraform ${var.username}"
-  visibility = "public"
+module "github_repository" {
+    source = "./modules"
+    
+    
+  
 }
