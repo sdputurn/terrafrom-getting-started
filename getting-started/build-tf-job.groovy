05cb1a18e893fa09e1ba5cdeb37ed512e8e75e56
sdputurn <sdp.uturn@gmail.com>
2023-11-14 01:12:50 +0530

    added job DSL

---
+++ getting-started/build-tf-job.groovy
@@ -0,0 +1,15 @@
+pipelineJob('build-tf-job') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/sdputurn/terrafrom-getting-started.git')
+          }
+          branch('*/master')
+        }
+      }
+      lightweight()
+    }
+  }
+}
