2c30f1d96a7ef72fb99f8d017ab6bb3ba78b81d5
sdputurn <sdp.uturn@gmail.com>
2023-11-15 20:10:06 +0530

    added lib example and job dsl example

--- getting-started/build_tf_job.groovy
+++ getting-started/build_tf_job.groovy
@@ -1,5 +1,47 @@
 pipelineJob('build-tf-job') {
+  description("build tf code")
+  properties {
+        pipelineTriggers {
+            triggers {
+              cron {
+                spec('@midnight or MM HH DOM MONTH DAYOFWEEK(0-7)')
+              }
+            }
+        }
+    }
+  parameters {
+    stringParam('name','value','desc')
+  }
   definition {
+    // can be defined in 2 ways using cps and cpsScm.
+    // in cps you need to define the script of the pipeline like Jenkinsfile. or you can write whole script or reference to a file
+    // cps example
+    // cps {
+    //   script('''
+    //     pipeline {
+    //         agent any
+    //             stages {
+    //                 stage('Stage 1') {
+    //                     steps {
+    //                         echo 'logic'
+    //                     }
+    //                 }
+    //                 stage('Stage 2') {
+    //                     steps {
+    //                         echo 'logic'
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //   '''.stripIndent())
+    //   sandbox()
+    // }
+    // example2 
+    // cps {
+    //   script(readFileFromWorkspace('file-seedjob-in-workspace.jenkinsfile'))
+    //   sandbox()     
+    // }
     cpsScm {
       scm {
         git {
